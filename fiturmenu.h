c4d8a5ba676b6961534f94511d2cbcde58692e84
Rhezapanjii <95005413+Rhezapanjii@users.noreply.github.com>
2022-06-06 22:11:27 +0700

    untuk tampilan help

---
+++ fiturmenu.h
@@ -0,0 +1,10 @@
+#ifndef Help_H
+#define Help_H
+
+void help(); 
+//untuk menampilkan cara untuk penggunaan teks editor
+
+void about();
+// untuk memberikan sedikit informasi terkait program teks editor
+
+#endif
